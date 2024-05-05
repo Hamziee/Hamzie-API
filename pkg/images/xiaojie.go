fb28d124cc0ddc66f01303aec0af86def2cedfe7
Hamza <hamza@heo-systems.com>
2024-05-05 22:10:58 +0200

    Create xiaojie.go

---
+++ pkg/images/xiaojie.go
@@ -0,0 +1,28 @@
+package images
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+)
+
+func generateXiaojieImages() []string {
+	var images []string
+	baseURL := "https://cdn.hamzie.us.to/Hamzie-API/images/cats/%d.webp"
+
+	for i := 1; i <= 130; i++ {
+		imageURL := fmt.Sprintf(baseURL, i)
+		images = append(images, imageURL)
+	}
+
+	return images
+}
+
+var XiaojieImages = generateXiaojieImages()
+
+func GetRandomXiaojieImage(w http.ResponseWriter, r *http.Request) {
+	randomIndex := rand.Intn(len(XiaojieImages))
+	randomCat := XiaojieImages[randomIndex]
+
+	http.Redirect(w, r, randomCat, http.StatusFound)
+}
